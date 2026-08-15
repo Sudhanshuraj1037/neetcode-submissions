40ec19968fc76630a879f23f43de96634abbede8
Sudhanshu Raj <sudhanshu78158@gmail.com>
2026-08-15 23:43:57 +0530

    Add: majority-element - submission-8

---
+++ Data Structures & Algorithms/majority-element/submission-8.py
@@ -0,0 +1,22 @@
+from typing import List
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        # major={}
+        # n = len(nums)
+        # for i in nums:
+        #     if i not in major:
+        #         major[i] = 0
+        #     major[i] += 1
+        #     if major[i] > n//2:
+        #         return i
+
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            count += 1 if num == candidate else -1
+        return candidate
+
